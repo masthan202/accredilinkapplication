44975434daba5b9d1667106f7e0c66ff0d8e659a
Masthan <54172137+masthan202@users.noreply.github.com>
2019-11-03 14:37:46 +0530

    CodeCommit for accredilink crudoperations
    CodeCommit for accredilink crudoperations

---
+++ src/main/java/com/accredilink/bgv/service/BGVService.java
@@ -0,0 +1,26 @@
+package com.accredilink.bgv.service;
+
+import com.accredilink.bgv.dto.RegistrationDTO;
+import com.accredilink.bgv.entity.Registration;
+
+public interface BGVService {
+
+	/**
+	 * @param ssnNumber
+	 * @return
+	 */
+	public Registration view(Long ssnNumber);
+
+	/**
+	 * @param registrationDTO
+	 * @return
+	 * @throws Exception
+	 */
+	public Registration update(RegistrationDTO registrationDTO) throws Exception;
+
+	/**
+	 * @param ssnNumber
+	 */
+	public void delete(Long ssnNumber);
+
+}
---
+++ src/main/java/com/accredilink/bgv/service/RegistrationService.java
@@ -0,0 +1,21 @@
+package com.accredilink.bgv.service;
+
+import com.accredilink.bgv.dto.RegistrationDTO;
+
+public interface RegistrationService {
+
+	/**
+	 * @param registrationDTO
+	 * @return
+	 * @throws Exception
+	 */
+	public String registration(RegistrationDTO registrationDTO) throws Exception;
+
+	/**
+	 * @param userName
+	 * @param password
+	 * @return
+	 */
+	public String login(String userName, String password);
+
+}
